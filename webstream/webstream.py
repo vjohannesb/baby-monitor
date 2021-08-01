9703b0d8f1cf430412db59ec3669b69fe0b4557a
vjohannesb <johannes.b.nilsson@gmail.com>
2021-08-01 03:27:48 +0200

    no socket for now

--- webstream/webstream.py
+++ webstream/webstream.py
@@ -16,13 +16,14 @@ output_frame = None
 lock = threading.Lock()
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# socketio = SocketIO(app, cors_allowed_origins="*")
 vs = VideoStream(src=0).start()
 
 def get_frame():
     global vs, output_frame, lock
     while True:
         frame = vs.read()
+
         with lock:
             output_frame = frame.copy()
         
@@ -53,7 +54,7 @@ def video_feed():
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--ip", type=str, required=True, help="IP Address of the device")
-    ap.add_argument("-o", "--port", type=int, required=True, help="Port number of the server (1024 to 65535)")
+    ap.add_argument("-p", "--port", type=int, required=True, help="Port number of the server (1024 to 65535)")
     args = vars(ap.parse_args())
     print(args)
     
@@ -61,7 +62,8 @@ if __name__ == "__main__":
     t.daemon = True
     t.start()
 
-    socketio.run(app, host=args["ip"], port=args["port"], debug=False)
+    # socketio.run(app, host=args["ip"], port=args["port"], debug=False)
+    app.run(host=args["ip"], port=args["port"], debug=False)
 
 # Release VideoStream pointer
 # vs.stop()
